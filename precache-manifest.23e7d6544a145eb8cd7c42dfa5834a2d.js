2469e93f3605e5f231bc26235c8b7e163e1cec74
Matvey_L <liakh.matvei@mail.ru>
2020-05-14 10:40:10 +0300

    Updates

---
+++ precache-manifest.23e7d6544a145eb8cd7c42dfa5834a2d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7bdd43db0734fbb011171f3cdc02ea",
+    "url": "/bizpro/index.html"
+  },
+  {
+    "revision": "ba013a8e18185067237d",
+    "url": "/bizpro/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "55e9123da7b677925119",
+    "url": "/bizpro/static/css/main.5ef550fb.chunk.css"
+  },
+  {
+    "revision": "ba013a8e18185067237d",
+    "url": "/bizpro/static/js/2.b58e8f71.chunk.js"
+  },
+  {
+    "revision": "353c9a25281c3a781e097e759231414a",
+    "url": "/bizpro/static/js/2.b58e8f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e9123da7b677925119",
+    "url": "/bizpro/static/js/main.5d80744b.chunk.js"
+  },
+  {
+    "revision": "03b31121b628faf7f560",
+    "url": "/bizpro/static/js/runtime-main.c05f051b.js"
+  },
+  {
+    "revision": "a8256f9d5275f9335cf52fc55b8e2798",
+    "url": "/bizpro/static/media/earth2.a8256f9d.png"
+  },
+  {
+    "revision": "d8d58e95af11a45c09cd887cf9e7ae58",
+    "url": "/bizpro/static/media/rocket.d8d58e95.png"
+  }
+]);
